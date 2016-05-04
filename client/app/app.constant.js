2d07470aec794c036cf2b411c8da86795fc801f7
Ziv Levy <zivilevy@gmail.com>
2016-05-05 02:07:37 +0300

    Overlays

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -1,19 +1,9 @@
-(function (angular, undefined) {
-  'use strict';
+(function(angular, undefined) {
+'use strict';
 
-  angular.module('elbitApp.constants', [])
+angular.module('elbitApp.constants', [])
 
-    .constant('appConfig', {
-      appName: 'Elbit Intel Pro',
-      appVersion: '0.0.1',
-      userRoles: ['guest', 'user', 'admin'],
-      mapCenter: {
-        lat: 32.06250082836163,
-        lng: 34.79513347148895,
-        zoom: 13
-      }
+.constant('appConfig', {userRoles:['guest','user','admin']})
 
-    })
-
-  ;
+;
 })(angular);
--- client/app/mapMain/mapMain.controller.js
+++ client/app/mapMain/mapMain.controller.js
@@ -50,6 +50,38 @@
     };
 
     initMap() {
+      var markers = {
+        London : {
+          lat: 51.50,
+          lng: -0.082,
+          draggable: false
+        },
+        Manchester: {
+          lat: 53.48,
+          lng: -2.24,
+          draggable: true
+        },
+        Lincoln: {
+          lat: 53.230495,
+          lng: -0.53936,
+          draggable: true
+        },
+        Northhampton: {
+          lat: 52.237892,
+          lng: -0.90087,
+          draggable: true
+        },
+        Worcester: {
+          lat: 52.187404,
+          lng: -2.20275,
+          draggable: true
+        },
+        York: {
+          lat: 53.959317,
+          lng: -1.08215,
+          draggable: true
+        }
+      };
       angular.extend(this.$scope, {
         events: {
           map: {
@@ -57,18 +89,136 @@
             logic: 'emit'
           }
         },
-        center: this.appConfig.mapCenter,
         defaults: {
           tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
           maxZoom: 18,
-          minZoom: 5,
+          minZoom: 0,
           tileLayerOptions: {
             detectRetina: true,
-            reuseTiles: true,
+            reuseTiles: false,
           },
           scrollWheelZoom: true
+        },
+        markers: markers,
+        center: {
+          lat: 32.06250082836163,
+          lng: 34.79513347148895,
+          zoom: 13
+        },
+        layers: {
+          baselayers: {
+            osm: {
+              name: 'OpenStreetMap',
+              url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              type: 'xyz'
+            },
+            satellite: {
+              name: 'Satellite',
+              url: 'https://1.aerial.maps.cit.api.here.com/maptile/2.1/maptile/newest/satellite.day/{z}/{x}/{y}/256/png8?app_id=cNjOx0DxhSTwsngVa9My&app_code=CQ45DHJ3KvXpHjUoLwjTVg',
+              type: 'xyz',
+              layerOptions: {}
+            },
+            grayscale: {
+              name: 'Grayscale',
+              url: 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
+              type: 'xyz',
+              layerOptions: {}
+            }
+          },
+          overlays:{
+            lines: {
+              name: 'Lines',
+              type: 'group',
+              visible: true
+            },
+            shapes: {
+              name: 'Shapes',
+              type: 'group',
+              visible: false
+            },
+            fire: {
+              name: 'OpenFireMap',
+              type: 'xyz',
+              url: 'http://openfiremap.org/hytiles/{z}/{x}/{y}.png',
+              layerOptions: {
+                attribution: '&copy; <a href="http://www.openfiremap.org">OpenFireMap</a> contributors - &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                continuousWorld: true
+              }
+
+          }
+,
+
+
         }
+      },
+        paths: {
+          p1: {
+            color: '#008000',
+            weight: 4,
+            latlngs: [ markers.London, markers.Manchester ],
+            layer: 'lines'
+          },
+          p2: {
+            weight: 3,
+            opacity: 0.5,
+            latlngs: [
+              [ markers.London, markers.Lincoln ],
+              [ markers.Manchester, markers.Worcester]
+            ],
+            type: 'multiPolyline',
+            layer: 'lines'
+
+          },
+          c1: {
+            weight: 2,
+            color: '#ff612f',
+            latlngs: markers.Northhampton,
+            radius: 10000,
+            type: 'circle',
+            layer: 'shapes'
+          },
+          c2: {
+            weight: 2,
+            color: '#ff612f',
+            latlngs: markers.Lincoln,
+            radius: 50,
+            type: 'circleMarker',
+            layer: 'shapes'
+          },
+          pg1: {
+            latlngs: [ markers.London, markers.Worcester, markers.Lincoln ],
+            stroke: false,
+            fillColor: '#ff69b4',
+            type: 'polygon',
+            layer: 'shapes'
+          },
+          pg2: {
+            weight: 1,
+            color: '#2e3974',
+            latlngs: [
+              [ markers.London, markers.Worcester, markers.Northhampton ],
+              [ markers.Manchester, markers.Lincoln, markers.York ]
+            ],
+            type: 'multiPolygon',
+            layer: 'shapes'
+          },
+          r1: {
+            latlngs: [ markers.Lincoln, markers.York ],
+            type: 'rectangle',
+            layer: 'shapes'
+          }
+        }});
+
+
+      this.leafletData.getMap('mymap').then(map=> {
+
+
       });
+
+
+
+
+
     };
 
     setMapEvents() {
